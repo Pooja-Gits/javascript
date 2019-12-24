e97c4b3aec2d56494c5677394d53ae4a79a6cbcc
Manoj Bhargavan <manojbhargavan07@gmail.com>
2019-12-24 07:35:28 +0530

    small prac

--- Practice/Events/inputEvent/app.js
+++ Practice/Events/inputEvent/app.js
@@ -1,15 +1,18 @@
-const inputs = [
+let inputs = [
     document.querySelector("#userName"),
     document.querySelector("#termsCheck"),
     document.querySelector("#region")
 ];
 
 let formData = {};
-
+let pData = document.querySelector("#data");
 for (let input of inputs) {
     input.addEventListener("input", function (e) {
         formData[e.target.name] = e.target.type === "checkbox" ? e.target.checked : e.target.value;
-        console.log(formData);
+        let curData = "";
+        for (let dataPoint of Object.keys(formData)) {
+            curData += `${dataPoint}:  ${formData[dataPoint]}\n`;
+        }
+        pData.innerText = curData;
     });
-
 }
